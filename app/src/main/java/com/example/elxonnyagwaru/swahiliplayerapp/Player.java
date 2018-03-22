35f2d440ecaf33b07ae6ea9552c3fd38589d1eb4
elxon <elianitunga@gmail.com>
2018-03-22 12:44:55 +0300

    playing music

--- app/src/main/java/com/example/elxonnyagwaru/swahiliplayerapp/Player.java
+++ app/src/main/java/com/example/elxonnyagwaru/swahiliplayerapp/Player.java
@@ -5,17 +5,38 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 import java.io.File;
 import java.util.ArrayList;
 
-public class Player extends AppCompatActivity {
-     MediaPlayer mp;
+public class Player extends AppCompatActivity implements View.OnClickListener   {
+     static MediaPlayer mp;
      ArrayList<File> mysongs;
+     SeekBar sb;
+     Uri u;
+     Button  btplay,btnext,btprev;
+     int position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player);
+         setContentView(R.layout.activity_player);
+         btplay =(Button) findViewById(R.id.play);
+         btprev = (Button) findViewById(R.id.prev);
+         btnext = (Button) findViewById(R.id.next);
+
+
+         btplay.setOnClickListener(this);
+         btnext.setOnClickListener(this);
+         btprev.setOnClickListener(this);
+
+         if (mp!=null){
+             mp.stop();
+             mp.release();
+         }
+
         Intent i=getIntent();
         Bundle  b= i.getExtras();
         mysongs =(ArrayList) b.getParcelableArrayList("songlist");
@@ -24,7 +45,44 @@ public class Player extends AppCompatActivity {
         mp=MediaPlayer.create(getApplicationContext(),u);
         mp.start();
 
+    }
 
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch(id){
+            case R.id.play:
+                if(mp.isPlaying()){
+                    btplay.setText("play");
+                   mp.pause();
+                }
+                else {
+                    btplay.setText("stop");
+                    mp.start();
+                }
+
+                break;
+
+            case R.id.next:
+                mp.stop();
+                mp.release();
+                position =(position+1)%mysongs.size();
+                Uri u = Uri.parse(mysongs.get(position).toString());
+                mp=MediaPlayer.create(getApplicationContext(),u);
+                mp.start();
+                break;
+                case R.id.prev:
+                    mp.stop();
+                    mp.release();
+                    position =(position -1<0)? mysongs.size()-1:position -1;
+                    u = Uri.parse(mysongs.get(position).toString());
+                    mp=MediaPlayer.create(getApplicationContext(),u);
+                    mp.start();
+                break;
+
+
+
+        }
 
     }
 }
