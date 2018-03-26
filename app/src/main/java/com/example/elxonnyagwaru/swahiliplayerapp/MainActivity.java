0ad6359e5a94ff659cedeb61be79f33b8fe4f874
elxon <elianitunga@gmail.com>
2018-03-26 18:17:15 +0300

    playing music

--- app/src/main/java/com/example/elxonnyagwaru/swahiliplayerapp/MainActivity.java
+++ app/src/main/java/com/example/elxonnyagwaru/swahiliplayerapp/MainActivity.java
@@ -4,36 +4,60 @@ import android.content.Intent;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.*;
 import android.widget.Toast;
-
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+
+import static java.util.Locale.ROOT;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv;
     String[] items;
+
+    ArrayList<String> mysongs;
+    ArrayAdapter<String> adp;
+    SearchView sv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv=(ListView) findViewById(R.id.lvp);
+        lv=findViewById(R.id.lvp);
+        sv=findViewById(R.id.search);
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String text) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String text) {
+                adp.getFilter().filter(text);
+                return false;
+            }
+        });
+
         final ArrayList<File> mysongs=findSongs(Environment.getExternalStorageDirectory());
+        //Collections.sort(mysongs);
         items = new String[ mysongs.size()];
         // ArrayList<File> songs=findSongs(Environment.getRootDirectory());
         for (int i=0;i<mysongs.size();i++){
-           // toast(mysongs.get(i).getName().toString());
-            items[i] = mysongs.get(i).getName().toString().replace(".mp3","").replace(".aac","");
+          //  toast(mysongs.get(i).getName().toString());
+            items[i] = mysongs.get(i).getName().toString().replace(".mp3","").replace(".aac","").replace(".m4a","");
         }
         ArrayAdapter<String> adp = new ArrayAdapter<String>(getApplication(),R.layout.songlist,R.id.textView,items);
+
         lv.setAdapter(adp);
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
               startActivity(new Intent(getApplicationContext(),Player.class).putExtra("pos",position).putExtra("songlist",mysongs));
+
             }
         });
     }
@@ -46,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
             else{
-                if (singlefile.getName().endsWith(".mp3")|| singlefile.getName().endsWith(".aac")){
+                if (singlefile.getName().endsWith(".mp3")|| singlefile.getName().endsWith(".aac")|| singlefile.getName().endsWith(".m4a")){
                     al.add(singlefile);
                 }
 
--- app/src/main/java/com/example/elxonnyagwaru/swahiliplayerapp/Player.java
+++ app/src/main/java/com/example/elxonnyagwaru/swahiliplayerapp/Player.java
@@ -1,5 +1,6 @@
 package com.example.elxonnyagwaru.swahiliplayerapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -12,20 +13,61 @@ import android.widget.SeekBar;
 import java.io.File;
 import java.util.ArrayList;
 
+
 public class Player extends AppCompatActivity implements View.OnClickListener   {
      static MediaPlayer mp;
      ArrayList<File> mysongs;
      SeekBar sb;
      Uri u;
+
+     Thread update;
      Button  btplay,btnext,btprev;
      int position;
+
+
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_player);
-         btplay =(Button) findViewById(R.id.play);
-         btprev = (Button) findViewById(R.id.prev);
-         btnext = (Button) findViewById(R.id.next);
+
+        // myplayer=findViewById(R.id.myplayer);
+         btplay = findViewById(R.id.play);
+         btprev = findViewById(R.id.prev);
+         btnext = findViewById(R.id.next);
+
+
+
+         ///handling seekbar here by initializing it
+        sb=findViewById(R.id.sbplayer);
+        update=new Thread() {
+            @Override
+            public void run() {
+                int totalDuration = mp.getDuration();
+                int currentPosition = 0;
+               // sb.setMax(totalDuration);
+                while (currentPosition < totalDuration) {
+                    try {
+                        try{
+                        currentPosition = mp.getCurrentPosition();}
+                        catch (final Exception e){
+                            e.printStackTrace();
+                        }
+                        sleep(500);
+
+
+
+                        sb.setProgress(currentPosition);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+
+                    // super.run();
+                }
+            }
+        };
+
 
 
          btplay.setOnClickListener(this);
@@ -41,11 +83,39 @@ public class Player extends AppCompatActivity implements View.OnClickListener   
         Bundle  b= i.getExtras();
         mysongs =(ArrayList) b.getParcelableArrayList("songlist");
         int position = b.getInt("pos",0);
-        Uri u = Uri.parse(mysongs.get(position).toString());
+        u = Uri.parse(mysongs.get(position).toString());
         mp=MediaPlayer.create(getApplicationContext(),u);
         mp.start();
+        mp.setLooping(true);
+        sb.setMax(mp.getDuration());
+        update.start();
+
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean input) {
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+               mp.seekTo(seekBar.getProgress());
+            }
+        });
+
 
     }
+
+
+
+
+
 
     @Override
     public void onClick(View view) {
@@ -55,29 +125,33 @@ public class Player extends AppCompatActivity implements View.OnClickListener   
                 if(mp.isPlaying()){
                     btplay.setText("play");
                    mp.pause();
+                    mp.setLooping(true);
                 }
                 else {
                     btplay.setText("stop");
                     mp.start();
+                    mp.setLooping(true);
                 }
 
                 break;
 
             case R.id.next:
-                mp.stop();
+                mp.reset();
                 mp.release();
                 position =(position+1)%mysongs.size();
                 Uri u = Uri.parse(mysongs.get(position).toString());
                 mp=MediaPlayer.create(getApplicationContext(),u);
                 mp.start();
+                sb.setMax(mp.getDuration());
                 break;
                 case R.id.prev:
-                    mp.stop();
+                    mp.reset();
                     mp.release();
                     position =(position -1<0)? mysongs.size()-1:position -1;
                     u = Uri.parse(mysongs.get(position).toString());
                     mp=MediaPlayer.create(getApplicationContext(),u);
                     mp.start();
+                    sb.setMax(mp.getDuration());
                 break;
 
 
